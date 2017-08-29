5cc8a141a2488ec62d5986f15e11804ef596b6b6
Olle Wiklund <owl@live.se>
2017-08-29 17:54:54 +0200

    Added module mod_rewrite to apache install

--- Server/Debian/setup_LAMP.sh
+++ Server/Debian/setup_LAMP.sh
@@ -17,3 +17,4 @@ ln -s /usr/share/phpmyadmin /var/www/public_html/phpmyadmin
 chown www-data:www-data /var/www -R
 
 perl -pi -e 'BEGIN{$/=undef} s/(www>.*\n.*\n.*AllowOverride )None/$1All/g' /etc/apache2/apache2.conf
+a2enmod rewrite
