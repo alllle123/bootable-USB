6474fb148037b594e6e1c1d7ae3b263e44ffc53c
Olle Wiklund <owl@live.se>
2017-09-08 16:50:44 +0200

    Did some things

--- Server/Debian/setup.sh
+++ Server/Debian/setup.sh
@@ -14,15 +14,13 @@ firewall-cmd --zone=dmz --add-service=http --permanent
 firewall-cmd --zone=dmz --add-service=https --permanent
 firewall-cmd --reload
 
-userdel onlinegroup
-adduser onlinegroup_admin
 usermod -Ga sudo onlinegroup_admin
 mkdir /home/onlinegroup_admin/.ssh
 mkdir /home/onlinegroup_admin/.vim
 mkdir .vim
 wget https://raw.githubusercontent.com/Kabix1/bootable-USB/master/Server/configs/sensible.vim
 cp sensible.vim /home/onlinegroup_admin/.vim/
-cp sensbile.vim .vim/
+cp sensible.vim .vim/
 echo "source ~/.vim/sensible.vim" > .vimrc
 cp .vimrc /home/onlinegroup_admin/
 chown onlinegroup_admin:onlinegroup_admin /home/onlinegroup_admin/ -R
